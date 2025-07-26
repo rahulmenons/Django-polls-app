a8e408e339545ebb1c9456efb13cfd2c6169ddf3
Rahul <rahumenonl43@gmail.com>
2025-07-26 20:05:10 +0530

    Added a voting functionality and error handling

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form with an error
+        return render(request, 'polls/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Redirect to the results page to prevent double-posting
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# 3 Views to be created index(5 request questions),detail(one question and choice),results.
+#render takes template with dictionary and creates a webpage/HTML basically
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
